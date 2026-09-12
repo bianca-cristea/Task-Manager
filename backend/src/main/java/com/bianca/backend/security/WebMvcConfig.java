c9cdd6e452de20b8761d3bd06e3f685656d99b57
Cristea Bianca <bianca.cristea01@yahoo.com>
2026-09-12 19:49:38 +0300

    .

--- backend/src/main/java/com/bianca/backend/security/WebMvcConfig.java
+++ backend/src/main/java/com/bianca/backend/security/WebMvcConfig.java
@@ -1,9 +1,16 @@
 package com.bianca.backend.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebMvcConfig  {
-
+@Configuration
+public class WebMvcConfig  implements  WebMvcConfigurer{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:images/");
+    }
 }
